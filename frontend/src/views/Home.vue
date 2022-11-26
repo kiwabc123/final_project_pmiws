d22b6f22a65a01a130ea99a05aeda2ef8ab3de51
kiwabc123 <75328842+kiwabc123@users.noreply.github.com>
2022-11-26 16:40:45 +0700

    .

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,236 +1,199 @@
 <template>
-  <div class="app">
-    <apexcharts width="550" type="line" :options="chartOptions" :series="series"></apexcharts>
-    <div>
-      <button @click="updateChart">Update!</button>
-    </div>
-  </div>
+  <v-app light>
+    <Appbar></Appbar>
+    <v-main>
+      <section>
+        <v-parallax :src="imageLink.sub_main" height="600">
+          <v-layout column align-center justify-center class="white--text">
+            <h1 class="white--text mb-2 display-1 text-xs-center" style="font-weight: 900; text-shadow: 3px 2px #000000">The social network for epic content</h1>
+            <div class="white--text subheading mb-3 text-xs-center" style="font-weight: 900; text-shadow: 2px 2px #000000">Unlesh your creativity without limitations</div>
+            <v-btn class="blue lighten-2 mt-5" dark large href="/pre-made-themes">
+              Get Started
+            </v-btn>
+          </v-layout>
+        </v-parallax>
+      </section>
+
+      <section>
+        <v-layout
+          column
+          wrap
+          class="my-5"
+          align-center
+        >
+          <v-flex xs12 sm4 class="my-3">
+            <div class="text-xs-center">
+              <h2 class="headline">The best way to share your amazing stuff</h2>
+              <span class="subheading">
+                No more restrictions, no more limits
+              </span>
+            </div>
+          </v-flex>
+          <v-flex xs12>
+            <v-container grid-list-xl>
+              <v-layout row wrap align-center>
+                <v-flex xs12 md4>
+                  <v-card class="elevation-0 transparent">
+                    <v-card-text  class=" layout justify-center">
+                      <v-icon x-large class="blue--text text--lighten-2 ">mdi-earth</v-icon>
+                    </v-card-text>
+                    <v-card-title primary-title class="layout justify-center">
+                      <div class="headline text-xs-center">Reach the world</div>
+                    </v-card-title>
+                    <v-card-text>
+                      Show your stuff to the whole community of Endorfine not only to your mum or your friends. We love making good content viral. In this moment Endorfine is used by artists who are not famous but that want to share their works to the world. Unfortunately with other social networks this is hard, slow and sometime expensive.
+                    </v-card-text>
+                  </v-card>
+                </v-flex>
+                <v-flex xs12 md4>
+                  <v-card class="elevation-0 transparent">
+                    <v-card-text class="text-xs-center">
+                      <v-icon x-large class="blue--text text--lighten-2">flash_on</v-icon>
+                    </v-card-text>
+                    <v-card-title primary-title class="layout justify-center">
+                      <div class="headline">Fast feedback</div>
+                    </v-card-title>
+                    <v-card-text>
+                      "Time is important, we don't want you to waste it. Here you can get a massive feedback from real users in minutes. And if your stuff is appreciated you won't only get positive feedback but also lovely and sincere fans 
+                    </v-card-text>
+                  </v-card>
+                </v-flex>
+                <v-flex xs12 md4>
+                  <v-card class="elevation-0 transparent">
+                    <v-card-text class="text-xs-center">
+                      <v-icon x-large class="blue--text text--lighten-2">share</v-icon>
+                    </v-card-text>
+                    <v-card-title primary-title class="layout justify-center">
+                      <div class="headline text-xs-center">Create new connections</div>
+                    </v-card-title>
+                    <v-card-text>
+                      Imagine if you can directly speak with the world's population. Don't you think it would be easier to find nice people to interact with? Endorfine is both local and global and help you to connect without limitations with people from your city, your state and your universe! 
+                    </v-card-text>
+                  </v-card>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-flex>
+        </v-layout>
+      </section>
+
+    
+    
+       <section>
+        <v-parallax :src="imageLink.social_cover" height="380">
+          <v-layout column align-center justify-center>
+            <div class="headline white--text mb-3 text-xs-center">We are dropping cool news and opportunities on socials</div>
+          </v-layout>
+          <v-layout justify-space-around justify-center>
+
+              <v-icon x-large dark>mdi-facebook</v-icon>
+
+              <v-icon x-large dark>mdi-github</v-icon>
+
+              <v-icon x-large dark>mdi-web</v-icon>
+
+            </v-layout>
+        </v-parallax>
+      </section>
+
+      <section>
+        <v-container grid-list-xl>
+          <v-layout row wrap justify-center class="my-5">
+            <v-flex xs12 sm4>
+              <v-card class="elevation-0 transparent">
+                <v-card-title primary-title class="layout justify-center">
+                  <div class="headline">Company info</div>
+                </v-card-title>
+                <v-card-text>
+                  We are not a company. We hate companies. Just imagine us like the guys from the Silicon Valley series. 
+                </v-card-text>
+              </v-card>
+            </v-flex>
+            <v-flex xs12 sm4 offset-sm1>
+              <v-card class="elevation-0 transparent">
+                <v-card-title primary-title class="layout justify-center">
+                  <div class="headline">We are hiring</div>
+                </v-card-title>
+                <v-card-text>
+                  Are you a creative person? Do you like techy stuff? Complete the email form by writing your skills and interests
+                </v-card-text>
+              </v-card>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </section>
+
+     
+    
+
+
+   
+    </v-main>
+</v-app>
 </template>
 
+<script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
 <script>
-import VueApexCharts from 'vue-apexcharts'
-
+import Appbar from '@/components/Appbar'
 export default {
-  name: 'Chart',
   components: {
-    apexcharts: VueApexCharts,
+
+    Appbar
   },
-  data: function () {
+  name: "App",
+  data: function() {
     return {
-      chartOptions: {
-        chart: {
-          height: 350,
-          type: "line",
-          stacked: false
-        },
-        dataLabels: {
-          enabled: false
-        },
-        xaxis: {
-          type: 'numeric'
-        },
-        annotations: {
-
-          points: [{
-          
-            marker: {
-              size: 8,
-              fillColor: '#fff',
-              strokeColor: 'red',
-              radius: 2,
-              cssClass: 'apexcharts-custom-class'
-            },
-            label: {
-              borderColor: '#FF4560',
-              offsetY: 0,
-              style: {
-                color: '#fff',
-                background: '#FF4560',
-              },
-
-              text: 'Even Break Point ',
-            }
-          },
-          ]
-        },
-
+      title: "Endorfine",
+      imageLink: {
+        main:
+          "https://firebasestorage.googleapis.com/v0/b/endorfinevue.appspot.com/o/assets%2Fb13f0434-b228-11e6-8e5d-5252025056ab_web_scale_0.4666667_0.4666667__.jpg?alt=media&token=660df23e-599e-434b-9313-ba69c973eeea",
+        sub_main:
+          "https://firebasestorage.googleapis.com/v0/b/endorfinevue.appspot.com/o/assets%2FNight-Club-Clubbing-Jobs-Abroad2.jpg?alt=media&token=82bbda7d-5df4-430b-9217-adaf1c8485c5",
+        logo:
+          "https://firebasestorage.googleapis.com/v0/b/endorfinevue.appspot.com/o/assets%2Fandroid-chrome-512x512.png?alt=media&token=8a0a66f6-4741-4ff6-8f28-eb9ec74374df",
+        social_cover:
+          "https://firebasestorage.googleapis.com/v0/b/endorfinevue.appspot.com/o/assets%2Fo-NIGHTCLUB-facebook.jpg?alt=media&token=cefc5c4c-9714-41da-9c22-f63caf5e89a4"
       },
-      colors: ["#FF1654", "#247BA0"],
-      series: [
-        {
-          name: "Total cost",
-          // data: [2, 6, 10]
-          data: [{
-            x: 0,
-            y: 0
-          },
-          {
-            x: 25,
-            y: 25
-          },
-          {
-            x: 50,
-            y: 50
-          }],
+      email: "",
+      emailRules: [
+        v => {
+          return !!v || "E-mail is required";
         },
-        {
-          name: "Sales",
-          // data: [0, 6, 12]
-          data: [{
-            x: 0,
-            y: 10
-          }, {
-            x: 25,
-            y: 25
-          },
-
-          {
-            x: 50,
-            y: 40
-          }],
-        }
+        v =>
+          /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) ||
+          "E-mail must be valid"
       ],
-
-      yaxis: [
-        {
-          axisTicks: {
-            show: true
-          },
-          axisBorder: {
-            show: true,
-            color: "#FF1654"
-          },
-          labels: {
-            style: {
-              colors: "#FF1654"
-            }
-          },
-          title: {
-            text: "Series A",
-            style: {
-              color: "#FF1654"
-            }
-          }
-        },
-        {
-          opposite: true,
-          axisTicks: {
-            show: true
-          },
-          axisBorder: {
-            show: true,
-            color: "#247BA0"
-          },
-          labels: {
-            style: {
-              colors: "#247BA0"
-            }
-          },
-          title: {
-            text: "Series B",
-            style: {
-              color: "#247BA0"
-            }
-          }
-        }
-
-      ],
-
-
-
-      tooltip: {
-        shared: false,
-        intersect: true,
-        x: {
-          show: false
-        }
-      },
-      legend: {
-        horizontalAlign: "left",
-        offsetX: 40
-      }
-    }
+      subscribed: false
+    };
   },
   methods: {
-    updateChart() {
-      // const max = 90;
-      // const min = 20;
-      // const newData = this.series[0].data.map(() => {
-      //   return Math.floor(Math.random() * (max - min + 1)) + min
-      // })
-      // In the same way, update the series option
-      // this.series = [{
-      //   data: newData
-      // }]
-      // this.chartOptions.annotations={points: [{
-      //       x: 28,
-      //       y: 420,
-      //       marker: {
-      //         size: 8,
-      //         fillColor: '#fff',
-      //         strokeColor: 'red',
-      //         radius: 2,
-      //         cssClass: 'apexcharts-custom-class'
-      //       },
-      //       label: {
-      //         borderColor: '#FF4560',
-      //         offsetY: 0,
-      //         style: {
-      //           color: '#fff',
-      //           background: '#FF4560',
-      //         },
+    subscribe: function() {
+      this.subscribed = !this.subscribed;
+    }
+  },
 
-      //         text: 'Even Break Point ',
-      //       }
-      //     },
-      //     ]}
-        console.log(this.chartOptions.annotations.points)
-        this.chartOptions.annotations.points[0].x=28
-        this.chartOptions.annotations.points[0].y=420
-      this.series = [{
-        name: "Total cost",
-        // data: [2, 6, 10]
-        data: [{
-          x: 0,
-          y: 0
-        },
-        {
-          x: 28,
-          y: 420
-        },
-        {
-          x: 56,
-          y: 840
-        }],
-      },
-      {
-        name: "Sales",
-        // data: [0, 6, 12]
-        data: [{
-          x: 0,
-          y: 70
-        }, {
-          x: 28,
-          y: 420
-        },
+  computed: {
+    imgHeight: function() {
+      var offset = 320;
+      console.log("new image height is " + (this.pageHeight - offset));
+      return this.pageHeight - offset;
+    }
+  },
 
-        {
-          x: 56,
-          y: 960
-        }],
-      }]
-    },
-
+  mounted: function() {
+    this.calculateHeight();
   }
-}
+};
 </script>
+
 <style scoped>
-button {
-  background: #26E6A4;
-  border: 0;
-  font-size: 16px;
-  color: '#fff';
-  padding: 10px;
-  margin-left: 28px;
+.finedTitle {
+  font-weight: 900;
+  text-shadow: 2px 2px #000000;
+}
+
+.social-icon {
+  font-size: 21px;
+  color: white;
 }
 </style>
