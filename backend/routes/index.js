b67bfbfcc68b050160811c9822df7cdcff24c9fc
kiwabc123 <75328842+kiwabc123@users.noreply.github.com>
2023-07-19 14:30:27 +0700

    Update index.js

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,16 +1,13 @@
 const router = require('express').Router()
 // const { authenticated, authenticatedAdmin } = require('../configs/security')
 
-const admin = require('./accounts/admin')
-const student = require('./accounts/student')
 
 const image = require('./image/image')
 const product = require('./product/product')
 const supplier =require('./supplier/supplier')
 
 
-router.use('/admin', admin)
-router.use('/students', student)
+
 
 router.use('/image',image)
 router.use('/product',product)
